09c8f859f856ab1e397400ddc3b3025f9c0f14ff
Ross Alexandra <ross-alexandra@outlook.com>
2025-01-15 09:25:53 -0800

    Update configs

--- keyboard/README.md
+++ keyboard/README.md
@@ -1,12 +1,12 @@
 ### Layout
-#### Layer 1
+#### Layer 1 - Primary typing
 ![alt text](screenshots/layer-1.png)
 
-#### Layer 2
+#### Layer 2 - Primary Actions
 ![alt text](screenshots/layer-2.png)
 
-#### Layer 3
+#### Layer 3 - Secondary Actions
 ![alt text](screenshots/layer-3.png)
 
-#### Layer 4
+#### Layer 4 - F-keys
 ![alt text](screenshots/layer-4.png)
--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,14 +1,15 @@
 # Generate with: code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension adrianwilczynski.alpine-js-intellisense
-code --install-extension biomejs.biome
 code --install-extension cardinal90.multi-cursor-case-preserve
 code --install-extension christian-kohler.path-intellisense
+code --install-extension daniloperes.relative-path-navigation
+code --install-extension dannyconnell.split-html-attributes
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension devonray.snippet
 code --install-extension donjayamanne.python-environment-manager
 code --install-extension donjayamanne.python-extension-pack
 code --install-extension eamodio.gitlens
 code --install-extension ecmel.vscode-html-css
+code --install-extension gencer.html-slim-scss-css-class-completion
 code --install-extension github.copilot
 code --install-extension github.copilot-chat
 code --install-extension golang.go
@@ -22,7 +23,6 @@ code --install-extension mblode.twig-language-2
 code --install-extension metaseed.metago
 code --install-extension metaseed.metajump
 code --install-extension metaseed.metaword
-code --install-extension mrmlnc.vscode-duplicate
 code --install-extension ms-playwright.playwright
 code --install-extension ms-python.black-formatter
 code --install-extension ms-python.debugpy
@@ -31,11 +31,16 @@ code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension otovo-oss.htmx-tags
-code --install-extension sankethdev.vscode-proto
+code --install-extension sleistner.vscode-fileutils
 code --install-extension sperovita.alpinejs-syntax-highlight
 code --install-extension spryinno.kanpilot
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension usernamehw.errorlens
+code --install-extension visualstudioexptteam.intellicode-api-usage-examples
+code --install-extension visualstudioexptteam.vscodeintellicode
 code --install-extension vue.volar
+code --install-extension vunguyentuan.vscode-css-variables
 code --install-extension waderyan.gitblame
+code --install-extension wenfangdu.snippet-generator
 code --install-extension wix.vscode-import-cost
+code --install-extension zobo.php-intellisense
