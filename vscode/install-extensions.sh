adcbe1d96f792ae2918616ac7b5236169c93b0e2
Ross Alexandra <Ross-Alexandra@outlook.com>
2025-07-20 11:36:41 -0700

    Update install-extensions.sh

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,5 +1,5 @@
-# Generate with: code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension adrianwilczynski.alpine-js-intellisense
+code --install-extension biomejs.biome
 code --install-extension cardinal90.multi-cursor-case-preserve
 code --install-extension christian-kohler.path-intellisense
 code --install-extension daniloperes.relative-path-navigation
@@ -29,18 +29,29 @@ code --install-extension ms-python.debugpy
 code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.vscode-python-envs
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension otovo-oss.htmx-tags
+code --install-extension redhat.java
+code --install-extension sankethdev.vscode-proto
 code --install-extension sleistner.vscode-fileutils
 code --install-extension sperovita.alpinejs-syntax-highlight
 code --install-extension spryinno.kanpilot
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension uctakeoff.vscode-counter
 code --install-extension usernamehw.errorlens
 code --install-extension visualstudioexptteam.intellicode-api-usage-examples
 code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension vscjava.vscode-gradle
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
 code --install-extension vue.volar
 code --install-extension vunguyentuan.vscode-css-variables
 code --install-extension waderyan.gitblame
 code --install-extension wenfangdu.snippet-generator
 code --install-extension wix.vscode-import-cost
 code --install-extension zobo.php-intellisense
+code --install-extension zxh404.vscode-proto3
